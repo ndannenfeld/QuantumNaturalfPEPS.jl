300d0aaffb7b740186876e9072858d7ab42b1c4a
Nicolas Dannenfeld <ndannenf@smail.uni-koeln.de>
2025-10-29 17:25:10 +0100

    removed the ITensors. overload

--- src/Environments.jl
+++ src/Environments.jl
@@ -18,8 +18,8 @@ Environment(::Type{S}, env::Environment) where S = Environment(convert_eltype.(S
 
 Base.getindex(env::Environment, i::Int) = env.env[i]
 Base.reverse(env::Environment) = ReverseEnvironment(env)
-ITensors.maxlinkdim(env::Environment) = maxlinkdim(env.env)
-ITensors.maxlinkdim(envs::Vector{Environment}) = maximum(maxlinkdim.(envs))
+maxlinkdim(env::Environment) = maxlinkdim(env.env)
+maxlinkdim(envs::Vector{Environment}) = maximum(maxlinkdim.(envs))
 
 struct ReverseEnvironment
     env::Environment
